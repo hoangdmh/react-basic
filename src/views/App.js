4cd9c9dc2077aac6d6a26000477c2b4171a1aeea
DGWHoang <hoang-dm@dgw.asia>
2023-11-02 15:19:26 +0700

    component in react

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.scss';
+import MyComponent from './example/MyComponent';
 
 function App() {
+// const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello world! I'm coming</p>
+        <MyComponent />
         <a
           className="App-link"
           href="https://reactjs.org"
---
+++ src/views/example/MyComponent.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MyFunction extends React.Component {
+    render(){
+        return(
+            <h2> Hi, I am Hoang</h2>
+        )
+    }
+}
+
+export default MyFunction;
