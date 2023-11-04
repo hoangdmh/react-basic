d2a12ad43a210670c4aa852231280e28287ad147
dmhoang <dmhoang08t1@gmail.com>
2023-11-04 15:43:31 +0700

    DOM Events

--- src/views/example/MyComponent.js
+++ src/views/example/MyComponent.js
@@ -1,9 +1,16 @@
 import React from 'react';
 
 class MyFunction extends React.Component {
+    handleClick = () => {
+        console.log('this is:');
+    };
+
     render(){
         return(
-            <h2> Hi, I am Hoang</h2>
+            <>
+                <h2> Hi, I am Hoang</h2>
+                <button onClick={this.handleClick}>Click me</button>
+            </>
         )
     }
 }
--- src/views/example/StateComponent.js
+++ src/views/example/StateComponent.js
@@ -16,7 +16,7 @@ class StateComponent extends React.Component {
         
         return(
             <>
-                <input type="text" class="form-control" value={this.state.name} onChange={this.handleChange} />
+                <input type="text" className="form-control" value={this.state.name} onChange={this.handleChange} />
                 <h3>Hi {this.state.name} {this.state['address']}</h3>
             </>
         )
